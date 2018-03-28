94938abb43bea408216329057b290577e5ce44e2
Gaurav Sood <soodoku@users.noreply.github.com>
2018-03-28 12:32:28 -0400

    karnataka done

---
+++ karnataka/karnataka_2018.py
@@ -0,0 +1,237 @@
+import requests
+from requests.adapters import HTTPAdapter
+from scrapy import Selector
+import csv
+import os
+
+# --------------------define variables-------------------
+OUTPUT_FILE = 'karnataka_2018.csv'
+PDF_FOLDER = 'karnataka_2018/'
+
+
+class KarnatakaScraper:
+    def __init__(self,
+                 base_url='http://ceokarnataka.kar.nic.in'
+                 ):
+        # define session object
+        self.session = requests.Session()
+        self.session.mount('https://', HTTPAdapter(max_retries=4))
+
+        # set proxy
+        # self.session.proxies.update({'http': 'http://127.0.0.1:40328'})
+
+        # define urls
+        self.base_url = base_url
+
+        self.form_data = {
+            '__EVENTVALIDATION': '',
+            '__VIEWSTATE': '',
+            '__VIEWSTATEENCRYPTED': ''
+        }
+
+    def GetDistrictList(self):
+        # set url
+        url = 'http://ceokarnataka.kar.nic.in/FinalRoll-2018/Dist_List.aspx'
+
+        # get request
+        ret = self.session.get(url)
+
+        if ret.status_code == 200:
+            # get district list
+            trs = Selector(text=ret.text).xpath('//table[@id="ctl00_ContentPlaceHolder1_GridView1"]/tr').extract()
+
+            district_list = []
+            for idx in range(1, len(trs)):
+                tr = trs[idx]
+                district = {
+                    'no': Selector(text=tr).xpath('//td[1]/font/text()').extract()[0],
+                    'name': Selector(text=tr).xpath('//td[2]/font/a/text()').extract()[0]
+                }
+                district_list.append(district)
+
+            return district_list
+        else:
+            print('fail to get district list')
+
+    def GetACList(self, district_no):
+        # set url
+        url = 'http://ceokarnataka.kar.nic.in/FinalRoll-2018/AC_List.aspx'
+
+        # set params
+        params = {
+            'DistNo': district_no
+        }
+
+        # get request
+        ret = self.session.get(url, params=params)
+
+        if ret.status_code == 200:
+            # get district list
+            trs = Selector(text=ret.text).xpath('//table[@id="ctl00_ContentPlaceHolder1_GridView1"]/tr').extract()
+
+            ac_list = []
+            for idx in range(1, len(trs)):
+                tr = trs[idx]
+                ac = {
+                    'no': Selector(text=tr).xpath('//td[1]/font/text()').extract()[0],
+                    'name': Selector(text=tr).xpath('//td[2]/font/a/text()').extract()[0]
+                }
+                ac_list.append(ac)
+
+            return ac_list
+        else:
+            print('fail to get ac list')
+
+    def GetPSList(self, ac_no):
+        # set url
+        url = 'http://ceokarnataka.kar.nic.in/FinalRoll-2018/Part_List_2018.aspx'
+
+        # set params
+        params = {
+            'ACNO': ac_no
+        }
+
+        # get request
+        ret = self.session.get(url, params=params)
+
+        if ret.status_code == 200:
+            # get district list
+            trs = Selector(text=ret.text).xpath('//table[@id="ctl00_ContentPlaceHolder1_GridView1"]/tr').extract()
+
+            ps_list = []
+            for idx in range(1, len(trs)):
+                tr = trs[idx]
+                ps = {
+                    'no': Selector(text=tr).xpath('//td[2]/font/text()').extract()[0],
+                    'name': Selector(text=tr).xpath('//td[3]/font/a/text()').extract()[0]
+                }
+                ps_list.append(ps)
+
+            return ps_list
+        else:
+            print('fail to get ps list')
+
+    def WriteHeader(self):
+        # set headers
+        header_info = [
+            'district_no',
+            'district_name',
+            'ac_no',
+            'ac_name',
+            'part_no',
+            'polling_station_name',
+            'filename'
+        ]
+
+        # write header into output csv file
+        writer = csv.writer(open(OUTPUT_FILE, 'w'), delimiter=',', lineterminator='\n')
+        writer.writerow(header_info)
+
+    def WriteData(self, data):
+        # write data into output csv file
+        writer = csv.writer(open(OUTPUT_FILE, 'a', encoding='utf-8'), delimiter=',', lineterminator='\n')
+        writer.writerow(data)
+
+    def DownloadPdfFile(self, download_url, filename):
+        print('downloading %s' % (filename))
+        if os.path.isfile(filename) == True:
+            print('this file already downloaded.')
+            return
+
+        # set url
+        url = download_url
+
+        # get request
+        ret = self.session.get(url, stream=True)
+
+        if ret.status_code == 200:
+            with open(filename, 'wb') as f:
+                f.write(ret.content)
+            print('success to download %s' % (filename))
+        else:
+            print('fail to get pdf file: %s' % (download_url))
+
+    def Start(self,
+              start_district='',
+              start_ac='',
+              start_ps=''):
+        # write header into output csv file
+        if start_district == '' and start_ac == '' and start_ps == '': self.WriteHeader()
+
+        # get district list
+        print('getting district list...')
+        district_list = self.GetDistrictList()
+        print(district_list)
+
+        district_flag = False
+        if start_district == '': district_flag = True
+
+        ac_flag = False
+        if start_ac == '': ac_flag = True
+
+        ps_flag = False
+        if start_ps == '': ps_flag = True
+
+        for district in district_list:
+            if start_district == district['no']: district_flag = True
+            if district_flag == False: continue
+
+            # get ac list
+            print('getting ac list for %s...' % (district['name']))
+            ac_list = self.GetACList(district['no'])
+            print(ac_list)
+
+            for ac in ac_list:
+                if start_ac == ac['no']: ac_flag = True
+                if ac_flag == False: continue
+
+                # get ps list
+                print('getting ac list for %s:%s...' % (district['name'], ac['name']))
+                ps_list = self.GetPSList(ac['no'])
+                print(ps_list)
+
+                for ps in ps_list:
+                    if start_ps == ps['no']: ps_flag = True
+                    if ps_flag == False: continue
+
+                    # process draft roll
+                    file_name = 'AC%s%s.pdf' % (
+                        str('{0:03d}'.format(int(ac['no']))),
+                        str('{0:04d}'.format(int(ps['no']))),
+                    )
+                    draft_roll_pdf_url = 'http://ceokarnataka.kar.nic.in/FinalRoll-2018/Kannada/AC%s/%s' % (
+                        str('{0:03d}'.format(int(ac['no']))),
+                        file_name
+                    )
+                    self.DownloadPdfFile(draft_roll_pdf_url, PDF_FOLDER + file_name)
+
+                    # write data into output csv file
+                    data = []
+                    data.append(district['no'])
+                    data.append(district['name'])
+                    data.append(ac['no'])
+                    data.append(ac['name'])
+                    data.append(ps['no'])
+                    data.append(ps['name'])
+                    data.append(file_name)
+                    self.WriteData(data)
+
+            #         break
+            #     break
+            # break
+
+
+def main():
+    # create scraper object
+    scraper = KarnatakaScraper()
+
+    # start to scrape
+    scraper.Start(
+        start_district='',
+        start_ac='',
+        start_ps=''
+    )
+
+
+if __name__ == '__main__':
+    main()
--- karnataka/readme.md
+++ karnataka/readme.md
@@ -1,31 +1,34 @@
 ## Karnataka Electoral Rolls
 
-The python script [karnataka_2018.py](karnataka_2018.py) iterates through the 2018 final electoral rolls posted at http://ceokarnataka.kar.nic.in/FinalRoll-2018/Dist_List.aspx and does two things:
+We have 48,412 files for 2018, 46,552 files for 2017, 46,550 for 2016, and 46,551 for 2015. 
 
-1. It produces [karnataka.csv](karnataka_2018.csv) (encoded in UTF-8) with the following columns: `district_no, district_name, ac_no, ac_name, part_no, polling_station_name, filename` 
+For files that we couldn't fetch (that end in a 404), the `filename` column in the respective CSV (links below) has a NULL. For 2017, we were able to fetch all the files. For 2015 and 2018, we couldn't fetch one file each. And for 2016, we couldn't fetch two files.
 
-2. Downloads all the pdfs to a local folder called `karnataka_2018/`
+### Scripts
 
-### Running the Script
+For each of the years 2015--2018, we wrote a python script (the scripts don't differ by much) that did two things:
+
+1. Created a CSV file (encoded in UTF-8) with the following columns: `district_no, district_name, ac_no, ac_name, part_no, polling_station_name, filename`. The filename for files that 404 are missing. 
+
+2. Downloaded all the PDFs to a local folder `karnataka_20XX`
+
+Links to the scripts:
+
+* [karnataka_2018.py](karnataka_2018.py) iterates through the 2018 final electoral rolls posted at http://ceokarnataka.kar.nic.in/FinalRoll-2018/Dist_List.aspx
+* [karnataka_2017.py](karnataka_2017.py) iterates over the 2017 final electoral rolls posted at http://ceokarnataka.kar.nic.in/FinalRoll-2017/
+* [karnataka_2016.py](karnataka_2016.py) iterates over the 2016 final electoral rolls posted at http://ceokarnataka.kar.nic.in/FinalRoll_2016/
+* [karnataka_2015.py](karnataka_2015.py) iterates over the 2015 final electoral rolls posted at http://ceokarnataka.kar.nic.in/FinalRoll_2015/
+
+Links to the CSVs:
+
+* [karnataka_2018.csv](karnataka_2018.csv) 
+* [karnataka_2017.csv](karnataka_2017.csv) 
+* [karnataka_2016.csv](karnataka_2016.csv) 
+* [karnataka_2015.csv](karnataka_2015.csv)
+
+### Running the Scripts
 
 ```
-pip install -r requirements_2018.txt
+pip install -r requirements.txt
 python karnataka_2018.py
 ```
-
-### 2017
-
-The python script [karnataka_2017.py](karnataka_2017.py) iterates over the 2017 draft electoral rolls posted at http://ceokarnataka.kar.nic.in/DraftRolls_2017/ and does two things:
-
-1. It produces [karnataka_2017.csv](karnataka_2017.csv) (encoded in UTF-8) with the following columns: `district_no, district_name, ac_no, ac_name, part_no, polling_station_name, filename` 
-
-2. Downloads all the pdfs to a local folder called `karnataka_2017/`
-
-```
-pip install -r requirements_2017.txt
-python karnataka_2017.py
-```
-
-### Data
-
-We have X,XXX files for 2018 and Y,YYY files for 2017.
